9b0b52cca44990d37cd4c9c3337b16ff97ffff14
FranklinSolano <franklinsolano40@gmail.com>
2023-02-08 15:02:24 -0300

    terminado tela newAlert e inicio da tela Productions e ProductionsAlert

---
+++ AppGerenciarGastos/ProductionsAlert/View/ProductoinsAlertScreen.swift
@@ -0,0 +1,20 @@
+//
+//  ProductoinsAlertScreen.swift
+//  AppGerenciarGastos
+//
+//  Created by Franklin  Stilhano on 2/5/23.
+//
+
+import UIKit
+
+class ProductoinsAlertScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
